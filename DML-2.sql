5d48dc54bf665c907bb334abbb9dbadcc2cf21ee
dwadasi siva teja <sivateja1034@gmail.com>
2024-01-01 12:42:59 +0530

    Siva/basic syntax (#5)
    * added thge changes
    
    * added the chnages
    
    * created the dml
    
    ---------
    
    Co-authored-by: 12sivateja <sivateja.d@sreenidhi1sports.com>

---
+++ DML-2.sql
@@ -0,0 +1,25 @@
+-- data manipulation 
+show databases;
+use hr;
+
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1400,'2014 Jabberwocky Rd','26192','Southlake','Texas','US');
+
+-- INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1400,'2014 Jabberwocky Rd','26192','Southlake','Texas','US');
+INSERT INTO departments(department_id,department_name,location_id) VALUES (6,'IT',1400);
+
+commit ;
+INSERT INTO employees(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (103,'Alexander','Hunold','alexander.hunold@sqltutorial.org','590.423.4567','1990-01-03',9,9000.00,102,6);
+
+INSERT INTO employees(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (104,'Bruce','Ernst','bruce.ernst@sqltutorial.org','590.423.4568','1991-05-21',9,6000.00,103,6);
+INSERT INTO employees(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (105,'David','Austin','david.austin@sqltutorial.org','590.423.4569','1997-06-25',9,4800.00,103,6);
+INSERT INTO employees(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (106,'Valli','Pataballa','valli.pataballa@sqltutorial.org','590.423.4560','1998-02-05',9,4800.00,103,6);
+INSERT INTO employees(employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,manager_id,department_id) VALUES (107,'Diana','Lorentz','diana.lorentz@sqltutorial.org','590.423.5567','1999-02-07',9,4200.00,103,6);
+
+
+commit ;
+
+select * from departments;
+select * from employees order by salary  DESC;
+
+select e.first_name , d.department_name from employees as e , departments as d WHERE  e.department_id=d.department_id;
+commit ;
