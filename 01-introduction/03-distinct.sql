e21024b32a8708a1bf462422b25562adfea9c423
12sivateja <sivateja.d@sreenidhi1sports.com>
2024-01-30 17:02:37 +0530

    added the changes

---
+++ 01-introduction/03-distinct.sql
@@ -0,0 +1,12 @@
+-- selection of the distinction
+use hr;
+select * from employees;
+
+
+-- select distinct managers from employees;
+select distinct manager_id from employees;
+
+
+-- selecting the distict salary from the employees;
+select DISTINCT salary from employees;
+
