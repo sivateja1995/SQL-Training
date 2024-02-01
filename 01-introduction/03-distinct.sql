0fd73559eb36ce988eb5eb8516521aa329a6bd03
dwadasi siva teja <137869717+12sivateja@users.noreply.github.com>
2024-02-01 23:15:26 +0530

    distinct (#8)
    * added file structure changes
    
    * added changes
    
    * added changes in read me
    
    * added changes in read me
    
    * added the changes
    
    * added the changs

--- 01-introduction/03-distinct.sql
+++ 01-introduction/03-distinct.sql
@@ -10,3 +10,17 @@ select distinct manager_id from employees;
 -- selecting the distict salary from the employees;
 select DISTINCT salary from employees;
 
+-- selecting the distinct job and salary 
+select distinct job_id,salary 
+from employees
+order by job_id, salary desc;
+
+
+-- checking total number fo the employyes count
+select count(*) from employees;
+
+
+-- checking of the distinct phone numbers
+SELECT DISTINCT phone_number
+from employees
+order by phone_number;
