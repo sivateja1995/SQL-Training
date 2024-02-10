6be2c6dfec8e0a8583d6a8179cfeb08e631bb258
dwadasi siva teja <sivateja1034@gmail.com>
2024-02-10 23:46:59 +0530

    added the aggregateons (#19)
    Co-authored-by: = <=>

---
+++ 05-aggregations/aggregations.sql
@@ -0,0 +1,27 @@
+use hr;
+
+
+-- department wise average salary ADD
+select department_name,ROUND(AVG(salary),0) as avg_salary from employees 
+inner join departments using(department_id) 
+GROUP BY department_name 
+order by department_name;
+
+-- department wise min salary 
+select department_name,min(salary) as min_salary from employees 
+inner join departments using(department_id) 
+GROUP BY department_name 
+order by department_name;
+
+-- department wise max salary 
+select department_name,max(salary) as max_salary from employees 
+inner join departments using(department_id) 
+GROUP BY department_name 
+order by department_name;
+
+
+--- finding the head count 
+select department_name,count(*) as headcount from employees 
+inner join departments using(department_id) 
+GROUP BY department_name 
+order by department_name;
