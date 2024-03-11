29d04170991508555bf97ed10de09b83140e70c7
dwadasi siva teja <sivateja1034@gmail.com>
2024-03-11 10:15:56 +0530

    added the subquery (#25)
    Co-authored-by: 12sivaeteja <sivateja.d@sreenidhi1sports.com>

--- 08-subquery/01-subquery.sql
+++ 08-subquery/01-subquery.sql
@@ -1,4 +1,5 @@
 ---sql in subquery ADD
+use hr;
 select * from departments where location_id=1700;
 
 
@@ -16,3 +17,30 @@ order by first_name,last_name;
 select employee_id,first_name,last_name
 from employees
 where department_id in (select department_id from departments where location_id=1700);
+
+
+-- fetching the details of the employee having the max-salary
+select * from employees where salary= (select max(salary) from employees);
+
+-- fetching the deatils of the employees who is having the salary above the average
+
+select * from employees
+where salary > (select avg(salary) from employees) order by employee_id;
+
+-- select the departments which has atleast has employees has earnging more than 10,000;
+select * from department_name;
+
+
+SELECT 
+    department_name
+FROM
+    departments d
+WHERE
+    EXISTS( SELECT 
+            1
+        FROM
+            employees e
+        WHERE
+            salary > 10000
+                AND e.department_id = d.department_id)
+ORDER BY department_name;
