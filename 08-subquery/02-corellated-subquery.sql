5810a947a5edb64291bd3069a37f51905cae6c3f
= <=>
2024-03-22 23:47:46 +0530

    added the chagnes

--- 08-subquery/02-corellated-subquery.sql
+++ 08-subquery/02-corellated-subquery.sql
@@ -9,15 +9,37 @@ where salary > (
         select avg(salary)
         from employees
     );
-
-
 -- find all employees whose salary is higher than the average salary of the employees in their departments
-
-select employee_id,first_name,last_name,salary,department_id 
+select employee_id,
+    first_name,
+    last_name,
+    salary,
+    department_id
 from employees e
 where salary > (
-    select avg(salary) from employees where department_id= e.department_id
-) order by 
-department_id,
+        select avg(salary)
+        from employees
+        where department_id = e.department_id
+    )
+order by department_id,
+    first_name,
+    last_name;
+-- fetching of the average salary department wise with department name
+select *
+from employees;
+select employee_id,
+    first_name,
+    last_name,
+    department_name,
+    salary, (
+        select round(avg(salary), 0)
+        from employees
+        where department_id = e.department_id
+    ) as avg_emp_salary
+from employees e 
+inner join departments as d
+on d.department_id = e.department_id 
+order by 
+department_name,
 first_name,
 last_name;
