1934e7f6e4e8dcbb32abe834883a605a206ac207
= <=>
2024-03-24 18:45:26 +0530

    added the changs

--- 09-manipulation/03-delete.sql
+++ 09-manipulation/03-delete.sql
@@ -7,3 +7,10 @@ select * from dependents;
 
 insert into dependents (first_name,last_name,relationship,employee_id) values ('xxx','xxx','xxx',192);
 insert into dependents (first_name,last_name,relationship,employee_id) values ('werwerw','werwrwrwr','werwrw',192);
+
+delete from dependents where dependent_id =34;
+select * from dependents;
+
+
+--- deleting of the  dependent 
+delete from dependents where dependent_id =35;
