124a5ee16a567a2cb7f4dece6f9a85d509c46ca3
= <=>
2024-03-24 18:44:54 +0530

    added the changes

---
+++ 09-manipulation/03-delete.sql
@@ -0,0 +1,9 @@
+--- deleting of the  values from the table 
+select count(*) from dependents where dependent_id=16;
+
+
+select * from dependents;
+
+
+insert into dependents (first_name,last_name,relationship,employee_id) values ('xxx','xxx','xxx',192);
+insert into dependents (first_name,last_name,relationship,employee_id) values ('werwerw','werwrwrwr','werwrw',192);
