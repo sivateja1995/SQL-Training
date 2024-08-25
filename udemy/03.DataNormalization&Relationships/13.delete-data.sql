45e444761842a2d48e801b3c52b9ed411b29ab8c
dwadasi siva teja <sivateja1034@gmail.com>
2024-08-25 21:58:04 +0530

    Topic/normalization relationship (#48)
    * aded the chnges
    
    * added the changes
    
    * added the inner join and filters
    
    * added more joins
    
    * added more joins
    
    * added the changes
    
    * added the changse
    
    * added the changes
    
    * added the changes
    
    * added the changes
    
    * added new book
    
    * added the changes
    
    ---------
    
    Co-authored-by: sivateja <sivateja.d@sreenidhi1sports.com>

--- udemy/03.DataNormalization&Relationships/13.delete-data.sql
+++ udemy/03.DataNormalization&Relationships/13.delete-data.sql
@@ -1,1 +1,16 @@
 delete from address where id = 12;
+
+select * from address;
+
+select * from cities;
+
+select * from users as u
+left join address as a on a.id = u.address_id
+left join cities as c on c.id = a.city_id;
+
+delete from cities where id = 6;
+
+-- trying to delete the address where id =5
+delete from address where id = 5;
+
+select * from users;
