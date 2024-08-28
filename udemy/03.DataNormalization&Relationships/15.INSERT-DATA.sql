f6563f1772c64f330ee6da6481b87fbc0cae5499
dwadasi siva teja <sivateja1034@gmail.com>
2024-08-28 23:40:26 +0530

    Topic/normalization relationship (#54)
    * aded the chnges
    
    * added the changes
    
    * added the inner join and filters
    
    * added more joins
    
    * added more joins
    
    * added the changes
    
    * added the changse
    
    * added the changes
    
    * added the changes
    
    * added the changes
    
    * added new book
    
    * added the changes
    
    * added new table
    
    * layouts of the tables are created
    
    * layouts of the tables are created
    
    * layouts of the tables are created
    
    * added the changs
    
    * added the changs
    
    * added the changes
    
    ---------
    
    Co-authored-by: sivateja <sivateja.d@sreenidhi1sports.com>

--- udemy/03.DataNormalization&Relationships/15.INSERT-DATA.sql
+++ udemy/03.DataNormalization&Relationships/15.INSERT-DATA.sql
@@ -1,1 +1,17 @@
 -- inserting data into the employees
+insert into
+    employees (
+        first_name,
+        last_name,
+        birth_date,
+        email
+    )
+values (
+        'Max',
+        'Schwarz',
+        '1989-06-10',
+        'max@test.com'
+    );
+
+--- inserting of the data into intranet_accounts 
+insert into intranet_accounts(email,password) values ('max123@test.com','ranger25');
