b23bbf75349b86ce181c0dfaea7f62de77ac42bf
dwadasi siva teja <sivateja1034@gmail.com>
2024-09-01 22:37:38 +0530

    Topic/normalization relationship (#59)
    * aded the chnges
    
    * added the changes
    
    * added the inner join and filters
    
    * added more joins
    
    * added more joins
    
    * added the changes
    
    * added the changse
    
    * added the changes
    
    * added the changes
    
    * added the changes
    
    * added new book
    
    * added the changes
    
    * added new table
    
    * layouts of the tables are created
    
    * layouts of the tables are created
    
    * layouts of the tables are created
    
    * added the changs
    
    * added the changs
    
    * added the changes
    
    * added the changes
    
    * added the changes
    
    * created the company database
    
    * added the changs
    
    * added the changes
    
    * added the changs
    
    ---------
    
    Co-authored-by: sivateja <sivateja.d@sreenidhi1sports.com>

--- udemy/03.DataNormalization&Relationships/20.query.sql
+++ udemy/03.DataNormalization&Relationships/20.query.sql
@@ -1,3 +1,38 @@
+select e.first_name, e.last_name, p.title
+from
+    employees as e
+    inner join project_employee as ep on ep.employee_id = e.id
+    inner join projects as p on p.id = ep.project_id;
+
+-- fetching of the teams and building
+
+select e.id, e.first_name, e.last_name, b.name as building, t.name as team
+from
+    employees as e
+    inner join teams as t on t.id = e.team_id
+    inner join company_building as b on t.building_id = b.id
+where
+    b.id = 2;
+
+select * from employees;
+
+insert into
+    employees (
+        first_name,
+        last_name,
+        birth_date,
+        email,
+        team_id
+    )
+values (
+        'seshi',
+        'vandana',
+        '2000-10-07',
+        'seshi.vandana@gmail.com',
+        3
+    );
+
+    select * from teams;
 select e.first_name,e.last_name,p.title from employees as e
 left join project_employee as ep on ep.employee_id = e.id 
 left join projects as p on p.id = ep.project_id; 
