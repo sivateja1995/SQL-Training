12287f2a5f619b27628d5c13f76fa127b6329a9a
sivateja <sivateja.d@sreenidhi1sports.com>
2024-09-01 22:33:10 +0530

    added the changs

---
+++ udemy/03.DataNormalization&Relationships/21.delete.sql
@@ -0,0 +1,15 @@
+-- delete of the data from company 
+delete from company_building where id = 2;
+
+
+delete from teams where id = 2;
+
+-- deletion of the employee with id =1 
+delete from employees where id=1;
+
+select * from employees  where id=1;
+
+select * from intranet_accounts where email='julie@test.com';
+
+select * from project_employee where employee_id =1 ;
+
