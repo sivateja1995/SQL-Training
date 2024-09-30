3af74e1c54c127b9d2e46242c29c909ba18851d2
sivateja <sivateja.d@sreenidhi1sports.com>
2024-09-30 19:19:34 +0530

    added the data

--- udemy/04.composite-keys/05.data.sql
+++ udemy/04.composite-keys/05.data.sql
@@ -1,9 +1,31 @@
 insert into
-    employees (first_name, last_name, email, supervisor_id)
+    employees (
+        first_name,
+        last_name,
+        email,
+        supervisor_id
+    )
 values
+    -- ('Manuel', 'Lorenz','manuel@yopmail.com', 1)
     (
-        'julie',
+        'Julie',
         'Barnes',
-        'julie.barnes@yopmail.com',
-        NULL
+        'julie@yopmail.com',
+        7
     );
+
+select *
+from employees as e1
+    inner join employees as e2 on e1.supervisor_id = e2.id;
+
+-- creating of the users table
+create table users (
+    id SERIAL PRIMARY key,
+    first_name varchar(300) NOT NULL
+);
+
+create table users_friends (
+    user_id INT REFERENCES users on DELETE CASCADE,
+    friend_id INT REFERENCES users on DELETE CASCADE,
+    PRIMARY KEY (user_id, friend_id)
+)
