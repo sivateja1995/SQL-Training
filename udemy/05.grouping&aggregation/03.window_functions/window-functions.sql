1c6350d83b71e97b65cff4da0a195b8bc4191c96
sivateja1995 <sivateja1034@gmail.com>
2024-11-18 22:31:36 +0530

    test

--- udemy/05.grouping&aggregation/03.window_functions/window-functions.sql
+++ udemy/05.grouping&aggregation/03.window_functions/window-functions.sql
@@ -1,3 +1,10 @@
 
 
 select booking_date, amount_tipped, sum(amount_tipped) over() from bookings;
+
+
+
+select booking_date,amount_tipped, sum(amount_tipped) over(partition by booking_date) from bookings;
+
+
+-- ranking 
