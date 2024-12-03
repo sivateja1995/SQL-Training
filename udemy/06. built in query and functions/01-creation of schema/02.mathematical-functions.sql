e87cb364b807b61570f213679ec59e9dff5b4fc1
12sivateja <sivateja.d@sreenidhi1sports.com>
2024-12-03 18:18:52 +0530

    added the changes in mathimatical functions

--- udemy/06. built in query and functions/01-creation of schema/02.mathematical-functions.sql
+++ udemy/06. built in query and functions/01-creation of schema/02.mathematical-functions.sql
@@ -1,5 +1,13 @@
-
-
 -- select  sum(price*billing_frequency) as annualRevenue from membership;
+
+-- ceil()
+-- floor()
+-- round()
+-- trunc() -- truncate()
+
+select
+    consumption,
+    round(consumption, 1) as rounded_consumption
+from membership;
 
 
